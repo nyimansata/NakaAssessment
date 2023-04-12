644b29d8af1bca0bf2c4a146209556782d1bcb3e
nyimansata <nyimansataf459@gmail.com>
2023-04-12 09:08:56 -0700

    save

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,24 +1,38 @@
 import {View, Text, TextInput, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native'
 import {Feather} from '@expo/vector-icons';
 import { data } from '../Components/Data';
-
+import { useState } from 'react';
 
 const HomeScreen = ({navigation}) => {
+    const [search, setSearch] = useState('')
     // navigation
     const goToDetails = (item) => {
            navigation.navigate('Detail', {user:item});
     }
+    const handleInput = (text) => {
+        setSearch(text)
+        // console.log(text)
+    }
+
+    const SerchForItems =  data.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <View style={Styles.container}>
         <Text style={Styles.text}>Recipes</Text>
         <View style={Styles.searchContainer}>
             <Feather name="search" size={24} color="black" />
-            <TextInput placeholder='Search' style={Styles.input} />
+            <TextInput placeholder='Search' 
+                       style={Styles.input}
+                       value={search}
+                       onChangeText={handleInput}
+            />
         </View>
         {/* items */}
         <View>
             <FlatList 
-                data={data}
+                data={SerchForItems}
                 renderItem={({item}) => {
                     // console.log(data)
                     return(
